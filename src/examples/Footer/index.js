1387616fa9532ddbef73664e5aaccf46d2bd01a4
rahulmisra2000 <rahulmisra2000@gmail.com>
2024-12-18 14:38:37 -0500

    Name change, got authorization from Creative Tim

--- src/examples/Footer/index.js
+++ src/examples/Footer/index.js
@@ -99,12 +99,12 @@ function Footer({ company, links }) {
 
 // Setting default values for the props of Footer
 Footer.defaultProps = {
-  company: { href: "https://www.creative-tim.com/", name: "Creative Tim" },
+  company: { href: "https://www.clearinsight.solutions", name: "ClearInsight Solutions" },
   links: [
-    { href: "https://www.creative-tim.com/", name: "Creative Tim" },
-    { href: "https://www.creative-tim.com/presentation", name: "About Us" },
-    { href: "https://www.creative-tim.com/blog", name: "Blog" },
-    { href: "https://www.creative-tim.com/license", name: "License" },
+    { href: "https://www.www.clearinsight.solutions/", name: "ClearInsight Solutions" },
+    { href: "https://www.www.clearinsight.solutions/presentation", name: "About Us" },
+    { href: "https://www.www.clearinsight.solutions/blog", name: "Blog" },
+    { href: "https://www.www.clearinsight.solutions/license", name: "License" },
   ],
 };
 
